3a0dfcb960930641fc828e57aeb1a2c8b84f71bd
surya.m <surya.m@phonepe.com>
2021-03-09 21:29:29 +0530

    fixed prize

--- src/pages/Prize.jsx
+++ src/pages/Prize.jsx
@@ -1,18 +1,54 @@
-import React from 'react';
+import React from "react";
 
-import prize from "../temp/prize.png"
+import prize from "../temp/prize_bg2.png";
+import prizeIcon from "../temp/prizes_icon.svg";
 
-const Prize = ()=>{
-    return <div>
-        <img style= {{ width: "100%",
-  height: "auto"}}src={prize} alt="Snow"></img>
-        {/* <Container style={{height:"50vh",background:"red",width:"100wh",margin:"0px"}}>
-            kcfnclvfn
-        </Container>
-        <Container>
-            fkjvkjdfn
-        </Container> */}
+const Prize = () => {
+  return (
+    <div className='cuscont'>
+      <img
+        className='coverbgimg'
+        src={prize}
+        alt='Snow'
+        style={{ width: "100%" }}
+      />
+      <div className='bottom-left'>Bottom Left</div>
+      <div className='top-left'>Top Left</div>
+      <div className='top-right'>Top Right</div>
+      <div className='bottom-right'>Bottom Right</div>
+      <div className='cuscent'>
+        <img id='przicon' src={prizeIcon} alt='Snow' />
+        <br />
+        <br />
+        <h3 id='prziconquote'>
+          {" "}
+          "Every nation has a message to deliver, a mission to fulfil, a destiny
+          to reach.
+        </h3>
+
+        <i id='prziconquotelast'>-Swami Vivkeananda</i>
+      </div>
     </div>
-}
 
-export default  Prize;
+    // <div className='maindiv'>
+    //   <div className='container2'>
+    //     <div className='boxprize'>
+    //       <img id='resc' src={prize} alt='Snow'></img>
+    //     </div>
+    //     <div className='box2prize'>
+    //       <div className='box2child' style={{ color: "white" }}></div>
+    //     </div>
+    //   </div>
+    // </div>
+    // <div>
+    //   <img
+    //     id='resc'
+
+    //     src={prize}
+    //     alt='Snow'
+    //   ></img>
+    // </div>
+  );
+};
+
+export default Prize;
